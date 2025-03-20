776dae39cdcc3f511ed129641ea43b53498c469a
Isaac Wade <isaac.wade@icr.ac.uk>
2025-03-20 15:15:00 +0000

    add bioservices to conda env yaml

--- snakemake_call.sh
+++ snakemake_call.sh
@@ -10,7 +10,6 @@ WORKFLOW_FILE="brava_hits_common_condition_check.smk"
 echo "Starting a run of Snakemake workflow..."
 snakemake --snakefile "$WORKFLOW_FILE" --cores "$MAX_JOBS" --jobs "$MAX_JOBS" --max-status-checks-per-second 0.01 \
     --keep-going \
-    -n \
     2>&1 | tee snakemake_run.log 
 
 echo "Run complete."	
