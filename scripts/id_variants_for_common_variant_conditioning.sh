a74e9c8b056b92bd0726f08ec834d7ea907deb57
Isaac Wade <isaac.wade@icr.ac.uk>
2025-04-14 14:12:47 +0100

    changed logic to condition on common variants also in gene of interest itself

--- scripts/id_variants_for_common_variant_conditioning.sh
+++ scripts/id_variants_for_common_variant_conditioning.sh
@@ -12,6 +12,7 @@ VARIANTS_LIST="${ENSEMBL_ID}_${BP_DISTANCE}_${MAF_COMMON}_list.txt"
 VARIANTS_COMMA="${ENSEMBL_ID}_${BP_DISTANCE}_${MAF_COMMON}_string.txt"
 
 # Expand the BED regions for query and filter to coding regions
+## include common variation within the gene of interest too 
 EXPANDED_BED="expanded_regions_${ENSEMBL_ID}.bed"
 awk -v BP_DISTANCE="$BP_DISTANCE" 'BEGIN {OFS="\t"} {
     start = $2 - BP_DISTANCE;
@@ -22,8 +23,8 @@ awk -v BP_DISTANCE="$BP_DISTANCE" 'BEGIN {OFS="\t"} {
     # Add "chr" prefix if chromosome is numeric
     if ($1 ~ /^[0-9]+$/) $1 = "chr" $1;
 
-    print $1, start, $2;  # Upstream region
-    print $1, $3, end;    # Downstream region
+    # Print the entire expanded region
+    print $1, start, end;
 }' "$ENSEMBL_ID.bed" | bedtools intersect -a stdin -b protein_coding_regions_hg38_no_padding_no_UTR_v47.bed > "$EXPANDED_BED"
 
 # Use bcftools to filter VCF by the expanded BED regions and MAF threshold
