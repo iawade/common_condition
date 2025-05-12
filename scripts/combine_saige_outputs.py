6e6f6e216fda3e9d314369a07fc1e38702a11a29
astheeggeggs <duncan.stuart.palmer@gmail.com>
2025-05-12 15:10:27 +0100

    updated combination of results rules

--- scripts/combine_saige_outputs.py
+++ scripts/combine_saige_outputs.py
@@ -19,20 +19,23 @@ for file in files:
         continue
     gene, trait, distance, maf_cutoff = match.groups()
 
-    # Read file
-    df = pd.read_csv(file, sep="\t")
+    if os.path.getsize('filename') == 0:
+        print("File is empty")
+    else:
+        # Read file
+        df = pd.read_csv(file, sep="\t")
+        # Add extracted metadata as new columns
+        df["Gene_sanity_check"] = gene
+        df["Trait"] = trait
+        df["MAF_cutoff_for_conditioning_variants"] = maf_cutoff
+        dfs.append(df)
 
-    # Add extracted metadata as new columns
-    df["Gene_sanity_check"] = gene
-    df["Trait"] = trait
-    df["MAF_cutoff_for_conditioning_variants"] = maf_cutoff
-
-    dfs.append(df)
-
-# Combine all dataframes
-combined_df = pd.concat(dfs, ignore_index=True)
-
-# Save to a new file
-combined_df.to_csv("brava_conditional_analysis_results.txt", sep="\t", index=False)
+if (len(dfs) > 0):
+    # Combine all dataframes
+    combined_df = pd.concat(dfs, ignore_index=True)
+    # Save to a new file
+    combined_df.to_csv("brava_conditional_analysis_results.txt", sep="\t", index=False)
+else:
+    open("brava_conditional_analysis_results.txt", 'a').close()
 
 print("Files merged and sorted. Output saved as 'brava_conditional_analysis_results.txt'.")
