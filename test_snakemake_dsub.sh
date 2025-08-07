342dc247e6f682922a8690f606a0e998f21ec2d4
Duncan <duncan.stuart.palmer@gmail.com>
2025-08-07 10:15:12 +0000

    starting plink brnach

---
+++ test_snakemake_dsub.sh
@@ -0,0 +1,25 @@
+
+# Path to the Snakemake workflow file
+cd ${SCRIPT_DIR}
+
+WORKFLOW_FILE="brava_hits_common_stepwise_condition_check.smk"
+ls ${WORKFLOW_FILE}
+echo "${HOME}/conda-envs"
+
+mkdir -p saige_outputs run_files
+
+CORES=$(nproc)
+
+# Generate a timestamped log file
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+LOGFILE="snakemake_run_${TIMESTAMP}.log"
+
+# Run Snakemake with the specified options
+echo "Starting a run of Snakemake workflow..."
+snakemake --snakefile "$WORKFLOW_FILE" --cores $CORES --jobs $CORES --max-status-checks-per-second 0.01 \
+    --keep-going --retries 3 --use-conda --conda-prefix "${HOME}/conda-envs" \
+    --rerun-incomplete --printshellcmds --verbose --forcerun all \
+    > "$LOGFILE" 2>&1
+
+echo "Run complete. Log saved to $LOGFILE"
+mv ${LOGFILE} ${OUTPUT}/
