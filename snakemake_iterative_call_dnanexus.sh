38f36e0d1dd9537a5a15f03252c2ed5e481e960f
astheeggeggs <duncan.stuart.palmer@gmail.com>
2025-09-04 15:32:41 +0100

    updated dnanexus script

--- snakemake_iterative_call_dnanexus.sh
+++ snakemake_iterative_call_dnanexus.sh
@@ -3,31 +3,14 @@
 source /opt/conda/etc/profile.d/conda.sh
 conda activate brava_hits_common_condition_check
 
-ln -s /opt/conda/envs ${SCRIPT_DIR}/envs
+ln -s /opt/conda/envs envs
 
-# Path to the Snakemake workflow file
-cd ${SCRIPT_DIR}
 # Make files present in same filepath, so that file locations
 # in the list files are relative to the script directory
-mkdir ${DATA_DIR}
-VCF_DIR="${DATA_DIR}/vcf/combined"
-mkdir -p ${VCF_DIR}
-mv ${VCF} ${VCF_DIR}
-mv ${VCF_CSI} ${VCF_DIR}
-mv ${GENE_TRAIT_PAIRS_TO_TEST} .
-mv ${HOME}/in/* ${DATA_DIR}/
-
-# move any already completed run information into the VM
-mv ${RUN_DIR} run_files
-mv ${SAIGE_DIR} saige_outputs
-
 mv uk-biobank_configs/${ANC}_config.yaml config.yaml
 Rscript scripts/create_chr_specific_filepaths.r --chr ${CHR}
 
 WORKFLOW_FILE="brava_hits_common_stepwise_condition_check.smk"
-
-mkdir -p saige_outputs run_files/bed
-
 CORES=$(nproc)
 
 # Generate a timestamped log file
