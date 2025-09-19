f679d2c146c650482115d6a73213319dee08a5ff
astheeggeggs <duncan.stuart.palmer@gmail.com>
2025-09-19 13:58:54 +0100

    testing final snakemake on RAP

---
+++ run_step2_final_group_tests_uk-biobank_dnanexus_applet.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source /opt/conda/etc/profile.d/conda.sh
+conda activate brava_hits_common_condition_check
+
+mv uk-biobank_configs/${ANC}_config.yaml config.yaml
+Rscript scripts/create_chr_specific_filepaths.r --chr ${CHR}
+
+# Path to the Snakemake workflow file
+WORKFLOW_FILE="step2_final_group_tests_plink.smk"
+
+mkdir -p final_saige_outputs final_run_files
+
+CORES=$(nproc)
+
+# Generate a timestamped log file
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+LOGFILE="snakemake_run_${TIMESTAMP}.log"
+
+# Run Snakemake with the specified options
+echo "Starting a run of Snakemake workflow..."
+snakemake --snakefile "$WORKFLOW_FILE" --cores $CORES --touch
+snakemake --snakefile "$WORKFLOW_FILE" --cores $CORES --jobs $CORES --latency-wait 60 \
+    --max-status-checks-per-second 0.01 --keep-going --rerun-incomplete \
+    --printshellcmds --verbose --rerun-triggers code input params software-env \
+     2>&1 | tee "$LOGFILE"
+
+echo "Run complete. Log saved to $LOGFILE"
