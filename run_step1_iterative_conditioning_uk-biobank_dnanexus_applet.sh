5fedb1c997692eb20f0d64279ab535b843801f1a
astheeggeggs <duncan.stuart.palmer@gmail.com>
2025-09-27 14:43:37 +0100

    increased cores

--- run_step1_iterative_conditioning_uk-biobank_dnanexus_applet.sh
+++ run_step1_iterative_conditioning_uk-biobank_dnanexus_applet.sh
@@ -21,7 +21,7 @@ rm brava_stepwise_conditional_analysis_results*.txt
 # Run Snakemake with the specified options
 echo "Starting a run of Snakemake workflow..."
 snakemake --snakefile "$WORKFLOW_FILE" --cores $CORES --touch
-snakemake --snakefile "$WORKFLOW_FILE" --cores 1 --jobs 1 --latency-wait 60 \
+snakemake --snakefile "$WORKFLOW_FILE" --cores $CORES --jobs $CORES --latency-wait 60 \
     --max-status-checks-per-second 0.01 --keep-going --rerun-incomplete \
     --printshellcmds --verbose --rerun-triggers code input params software-env \
      2>&1 | tee "$LOGFILE"
