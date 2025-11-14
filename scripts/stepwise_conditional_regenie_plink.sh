28f024fd65e3b7c3921a2744335f53c2691cedf3
astheeggeggs <duncan.stuart.palmer@gmail.com>
2025-11-14 11:40:10 -0500

    working on iterative conditioning in regenie

--- scripts/stepwise_conditional_regenie_plink.sh
+++ scripts/stepwise_conditional_regenie_plink.sh
@@ -71,37 +71,28 @@ if [ $(wc -l < ${PLINK}.bim) -gt 2 ]; then
   # Run the command
   "${cmd[@]}"
 
-  # # Obtain the top hit as the first conditioning marker
-  # topline=$(sort -g -k13,13 "${TMPFILE}" | head -n 2 | tail -1)
-  # cond_M=$(awk '{print $1":"$2":"$4":"$5}' <<< "$topline")
-  # P_top=$(awk '{print $13}' <<< "$topline")
-  # lowest_pos_id=$(sort -g -k2,2 "${TMPFILE}" | head -n 2 | tail -1 | awk '{print $1":"$2":"$4":"$5}')
+  # Obtain the top hit as the first conditioning marker
+  topline=$(sort -g -k12,12r "${TMPFILE}_${PHENOCOL}.regenie" | head -n 2 | tail -1)
+  cond_M=$(awk '{print "chr"$1":"$2":"$4":"$5}' <<< "$topline")
+  P_top=$(awk '{print $12}' <<< "$topline")
+  lowest_pos_id=$(sort -g -k2,2 "${TMPFILE}" | head -n 2 | tail -1 | awk '{print $1":"$2":"$4":"$5}')
 
-  # echo "Lowest Pvalue in the sumstats file"
-  # echo "${cond_M}: ${P_top}"
-  # echo "Pvalue for significance for conditioning"
-  # echo ${P_T}
-  # vars=2
+  echo "Lowest Pvalue in the sumstats file"
+  echo "${cond_M}: ${P_top}"
+  echo "Pvalue for significance for conditioning"
+  echo ${P_T}
+  P_T_log=$(awk -v v="${P_T}" 'BEGIN {print -log(v)/log(10)}')
 
-  # while [ ${cond_M} = ${lowest_pos_id} ]; do
-  #   echo "Weird edge case - SAIGE cannot condition on the first variant in the bim."
-  #   vars=$((vars+1))
-  #   topline=$(sort -g -k13,13 "${TMPFILE}" | head -n ${vars} | tail -1)
-  #   cond_M=$(awk '{print $1":"$2":"$4":"$5}' <<< "$topline")
-  #   P_top=$(awk '{print $13}' <<< "$topline")
-  # done
-  # vars=2
 
-  # # Add the top variant to the list of conditioning markers
-  # CONDITION=${cond_M}
-  # # intFlag=$(awk -v P_top="${P_top}" -v P_T="${P_T}" 'BEGIN{print (P_top<P_T)?1:0}')
-  # intFlag=$(python3 -c "print(1 if ${P_top} < ${P_T} else 0)")
+  # Add the top variant to the list of conditioning markers
+  # Write to a tmp file
+  echo ${cond_M} > ${TMPFILE}.cond.txt
+  intFlag=$(python3 -c "print(1 if ${P_top} > ${P_T} else 0)")
 
-  # while [ "${intFlag}" -eq 1 ]
-  # do
-  #   # Run the step2_SPAtests.R and redirect output to TMPFILE
-  #   cond_cmd=("${cmd[@]}" --condition="${CONDITION}")
-  #   "${cond_cmd[@]}"
+  if [ "${intFlag}" -eq 1 ]
+  do
+    cond_cmd=("${cmd[@]}" --condition-list="${TMPFILE}.cond.txt")
+    "${cond_cmd[@]}"
 
   #   ncol=$(awk '{print NF; exit}' "${TMPFILE}")
   #   if [ "$ncol" -eq 29 ]; then
@@ -154,24 +145,17 @@ if [ $(wc -l < ${PLINK}.bim) -gt 2 ]; then
 
   #   CONDITION_unordered="${CONDITION},${cond_M}"
   #   echo "conditioning..."
-    
-  #   # Write a small R script to ensure that the conditioning SNPs are 
-  #   # in order
-  #   CONDITION=$(Rscript scripts/sort_conditioning_snps.R --condition "${CONDITION_unordered}")
-  #   echo $CONDITION
 
   #   # intFlag=$(awk -v P_top="${P_top}" -v P_T="${P_T}" 'BEGIN{print (P_top<P_T)?1:0}')
   #   intFlag=$(python3 -c "print(1 if ${P_top} < ${P_T} else 0)")
   #   rm -f "${TMPFILE}"
-  # done
+  done
 else
   echo "No common variants present in the region" 
 fi
 
 # Note that the very last variant in "$CONDITION_unordered" is not significant any more after the final conditioning:
 # CONDITION=$(echo "$CONDITION_unordered" | awk -F',' 'NF>1 { for (i=1; i<NF; i++) printf "%s%s", $i, (i<NF-1 ? "," : "") }')
-CONDITION=""
-echo "${CONDITION}" > ${VARIANTS_COMMA}
 
 # # Move and compress the output
 # mv *regenie ${OUT}.regenie
